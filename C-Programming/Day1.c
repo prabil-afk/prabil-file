be76bce824a64b73143e35f77fb64885abecd189
prabil-afk <56592304+prabil-afk@users.noreply.github.com>
2019-10-15 19:37:54 +0545

    Add files via upload

---
+++ C-Programming/Day1.c
@@ -0,0 +1,136 @@
+#include<stdio.h>
+int main()
+{
+//     int r;
+//     float pi = 3.14, area,circum;
+// printf("enter radius");
+// scanf("%d",&r);
+// area = pi*r*r;
+// circum = 2*pi*r;
+// printf("area=%f \n circum=%.2f",area,circum);
+//     printf("prabil\n");
+//     return 0;
+ 
+// int p,t,r;
+
+//  float sp;
+//  printf("enter principal");
+//  scanf("%d",&p);
+//  printf("enter time");
+//  scanf("%d",&t);
+//  printf("enter rate");
+//  scanf("%",&r);
+//   sp = (p*t*r)/100;
+//  printf("sp = %f ",sp);
+//  return 0;
+// int a = 10,b = 5;
+//     a =a+b;
+//     b =a-b;
+//     a= a-b;
+//     printf("a = %d\n b = %d" ,a,b);
+// int c;
+// float k;
+// printf("enter centigrade");
+// scanf("%d",&c);
+// k = (1.8*c) + 32;
+// printf("k = %f",k);
+// return 0;
+// int a,b,c,h,e;
+// float sum,per;
+// printf("Enter number");
+// scanf("%d",&a);
+// printf("enter number");
+// scanf("%d",&b);
+// printf("enter number");
+// scanf("&d", &c);
+// printf("enter numbesr");
+// scanf("%d",&h);
+// printf("enter number");
+// scanf("%d",&e);
+// sum = (a+b+c+h+e);
+// per =(a+b+c+h+e*100)/(a+b+c+h+e);
+// printf("sum = %f\n per = %f " ,sum, per);
+
+// int a,b,sum;
+// printf("Enter value");
+// scanf("%d",&a);
+// printf("enter value");
+// scanf("%d",&b);
+// sum = a+b;
+// printf("sum = %d", sum);
+// if (sum%2 == 0)
+// {
+//     printf("\n number is even.");
+// }
+// int a = 10;
+// a = 5;
+// int A=a+a;
+// printf(" A=%d",A);
+// int pani,gummna,gf,friends;
+// printf("enter number\nenter value");
+// scanf("%d%d",&a,&b);
+//  y=(a<b)?a:b;
+//  printf("y = %d",y);
+// loop
+// int i;
+// for ( i = 0; i < 10; i++)
+// {
+//     printf("[%d] hello words \n",i+1);
+// }
+// int i = 1;
+    
+//     while (i < 10)
+//     {
+//         printf("%d\n", i);
+//         i++;
+
+//     }
+//     printf("%d is less than 10",i);
+//     return 0;
+ char operator;
+    double n1,n2;
+    printf("Enter  (+,-,/,*):");
+    scanf("%c" , &operator);
+    printf("Enter two number");
+    scanf("%lf%lf",&n1,&n2);
+    switch(operator)
+    {case'+':
+    printf("%.0lf+%..0f =%.1lf",n1,n2,n1+n2);
+    break;
+    case'-':
+    printf(".1%lf-%.1lf = %.1lf",n1,n2,n1-n2);
+    break;
+    case'/':
+    printf("%.1lf/%.1lf = %.1lf",n1,n2,n1/n2);
+    break;
+    printf("%.1lf*%.1lf = %.1lf",n1,n2,n1*n2);
+    default:
+    printf("error error error");
+    return 0;
+    }
+    
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
